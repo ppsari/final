8b58b118085f4f2daf409287ce96307ec5e8e5f5
stedyyulius <anthonyjuan95@gmail.com>
2017-07-13 10:58:06 +0700

    initial commit

--- reactVR/index.vr.js
+++ reactVR/index.vr.js
@@ -24,7 +24,7 @@ export default class reactVR extends React.Component {
             textAlignVertical: 'center',
             transform: [{translate: [0, 0, -3]}],
           }}>
-          hello
+          woi
         </Text>
       </View>
     );
