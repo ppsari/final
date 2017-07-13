ed83ff5a5ae3951de43061e5ca52e49f67b0398e
stedyyulius <stedyyulius@gmail.com>
2017-07-13 10:59:42 +0700

    initial commit

--- reactVR/index.vr.js
+++ reactVR/index.vr.js
@@ -16,7 +16,7 @@ export default class reactVR extends React.Component {
           style={{
             backgroundColor: '#777879',
             fontSize: 0.8,
-            fontWeight: '400',
+            fontWeight: '40',
             layoutOrigin: [0.5, 0.5],
             paddingLeft: 0.2,
             paddingRight: 0.2,
