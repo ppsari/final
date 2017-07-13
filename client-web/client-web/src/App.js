6febdc7c6d4d86e172aafc900c1e458540938f91
Priambodo-Kurniawan <iam@calibreworks.co.id>
2017-07-13 14:31:07 +0700

    routes on react

--- client-web/client-web/src/App.js
+++ client-web/client-web/src/App.js
@@ -1,14 +1,80 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch, Link, Redirect } from 'react-router-dom'
+
 import './App.css';
 import Register from './components/Register';
 import Login from './components/Login';
+import Home from './containers/Home';
+import Dashboard from './containers/protected/Dashboard';
+import { logout } from './helpers/auth';
+import { firebaseAuth } from './config/constants';
+import Page404 from './containers/Page404.js'
+
+function PrivateRoute ({component: Component, authed, ...rest}) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => authed === true
+        ? <Component {...props} />
+        : <Redirect to={{pathname: '/login', state: {from: props.location}}} />}
+    />
+  )
+}
+
+function PublicRoute ({component: Component, authed, ...rest}) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => authed === false
+        ? <Component {...props} />
+        : <Redirect to='/dashboard' />
+      }
+    />
+  )
+}
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      authed: false,
+      loading: true,
+    }
+  }
+
+  componentDidMount () {
+    this.removeListener = firebaseAuth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          authed: true,
+          loading: false,
+        })
+      } else {
+        this.setState({
+          authed: false,
+          loading: false,
+        })
+      }
+    })
+  }
+
+  componentWillUnmount () {
+    this.removeListener()
+  }
+
   render() {
-    return (
-      <div className="App">
-        <Register />
-      </div>
+    return this.state.loading === true ? <h1>Loading</h1> : (
+      <BrowserRouter>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <PublicRoute authed={this.state.authed} path='/login' component={Login} />
+            <PublicRoute authed={this.state.authed} path='/register' component={Register} />
+            <PrivateRoute authed={this.state.authed} path='/dashboard' component={Dashboard} />
+            <Route component={Page404} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ client-web/client-web/src/containers/Page404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => (
+  <div style={{
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: '100vh'
+  }}>
+    <h2>Page Not Found</h2>
+  </div>
+);
