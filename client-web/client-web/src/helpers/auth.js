eb6ab621d3d1c37f06c5b0b189ee7ede66130251
Priambodo-Kurniawan <iam@calibreworks.co.id>
2017-07-13 13:57:28 +0700

    login, register and social sign-in using firebase

---
+++ client-web/client-web/src/helpers/auth.js
@@ -0,0 +1,32 @@
+import { ref, firebaseAuth, uiConfig, ui } from '../config/constants'
+
+export function register (email, pw) {
+  return firebaseAuth().createUserWithEmailAndPassword(email, pw)
+  .then(saveUser)
+}
+
+export function logout () {
+  return firebaseAuth().signOut()
+}
+
+export function login (email, pw) {
+  return firebaseAuth().signInWithEmailAndPassword(email, pw)
+}
+
+export function resetPassword (email) {
+  return firebaseAuth().sendPasswordResetEmail(email)
+}
+
+export function saveUser (user) {
+  return ref.child(`users/${user.uid}/info`)
+    .set({
+      email: user.email,
+      uid: user.uid
+    })
+    .then(() => user)
+}
+
+// The start method will wait until the DOM is loaded.
+export function loadUi () {
+  return ui.start('#firebaseui-auth-container', uiConfig);
+}
