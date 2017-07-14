01994bf0a114506d42377dd32c0fd7642b26ffe3
stedyyulius <stedyyulius@gmail.com>
2017-07-14 12:18:04 +0700

    carousel rooms done

--- reactVR/index.vr.js
+++ reactVR/index.vr.js
@@ -8,7 +8,8 @@ import {
   VrButton,
   Image,
   Mesh,
-  VideoPano
+  VideoPano,
+  Sphere
 } from 'react-vr';
 
 export default class reactVR extends React.Component {
@@ -18,20 +19,28 @@ export default class reactVR extends React.Component {
       roomName: 'Chess',
       room: 'chess-world.jpg',
       index: 0,
+      desc: "",
+      isRead: false,
+      limit: 2,
+      space: 2,
+      start: 0,
       images:[{
           name: 'chess',
           image: 'chess-world.jpg',
-          type: 'bathRoom'
+          type: 'bathRoom',
+          description: 'aku chess'
         },
         {
           name: 'hacktiv',
           image:'PANO_20170712_204306_1.jpg.jpeg',
-          type: 'bedRoom'
+          type: 'bedRoom',
+          description: 'aku hacktiv'
         },
         {
           name: 'Pondok Indah Office Tower',
           image: 'PANO_20170713_094911_0.jpg.jpeg',
-          type: 'livingRoom'
+          type: 'livingRoom',
+          description: 'aku pim'
         }
       ],
       icons:
@@ -45,45 +54,77 @@ export default class reactVR extends React.Component {
     return (
       <View>
         <Pano source={asset(this.state.room)}/>
-         {/* {(this.state.index < this.state.images.length - 1)
-         ?<VrButton onClick={()=>this.move()}>
+           {this.state.images.slice(this.state.start,this.state.space).map((img,index)=> {
+            let icon = {uri: this.state.icons[img.type]};
+            let x = -3
+            let y = 0.9
+            let d = -3
+            return (
+              <View
+                style={{width: 0.5,
+                      height: 0.5,
+                      margin: 0.1,
+                      transform: [{translate: [x, y, d]}]}}
+                key={index}>
+              <VrButton onClick={()=>this.go(img,index)}>
+                <Image
+                source={icon}
+                style={{width: 0.5,
+                        height: 0.5}} />
+                <Text>{img.name}</Text>
+               </VrButton>
+             </View>)
+          })}
+        {(this.state.desc !== "")
+        ?<Text
+          style={{
+            color: 'white',
+            fontSize: 0.3,
+            fontWeight: '100',
+            layoutOrigin: [0.5, 0.5],
+            paddingLeft: 0.2,
+            paddingRight: 0.2,
+            textAlign: 'center',
+            textAlignVertical: 'center',
+            transform: [{translate: [3, 2,-1]},
+                        {rotateY: -45}]
+          }}>
+          {this.state.desc}
+         </Text>
+        :<VrButton></VrButton>
+        }
+        <VrButton onClick={()=> this.desc(this.state.index)}>
+          <Image
+            source={{uri:'https://thumbs.dreamstime.com/x/glassy-button-question-mark-24185998.jpg'}}
+            style={{width: 0.5,
+                    height: 0.5,
+                    transform: [{translate: [5.1, 2.5, 0]},
+                                {rotateY: -80}]}} />
+        </VrButton>
+        {(this.state.images.length > this.state.space)
+        ? <VrButton onClick={()=> this.next()}>
            <Image
-             source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrGUAHUDT6MyZfAnJkrOPoVsTX4x_00ohYJED-BajrskGGQ5iI_Q'}}
-             style={{width: 0.5,
-                     height: 0.5,
-                     transform: [{
-                       translate: [0, 2, -3]},
-                       {rotateY : 0},
-                       {rotateX : 0}]}} /></VrButton>
-         :<VrButton></VrButton>
+            source={{uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzzy_3OCG4HsujWkupN29XNKPBrvBAOftMsiQdIT-7r4MgC4RKXw'}}
+            style={{width: 0.3,
+                    height: 0.3,
+                    transform: [{translate: [-2.2, 2.5, -3]},
+                                {rotateY: 0}]}} />
+         </VrButton>
+        :<VrButton></VrButton>
        }
-       {(this.state.index > 0)
-       ? <VrButton onClick={()=>this.back()}>
-         <Image
-           source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNTqhTYiaJW9iU1d4Il5ihLupDAUiBSE3IVb22nJJhnB5LBuM_'}}
-           style={{width: 1,
-                   height: 1,
-                   transform: [{translate: [1, 1, 5]},
-                               {rotateY : 90},
-                               {rotateX : 0}]}} /></VrButton>
-       :<VrButton></VrButton>
-     } */}
-     {this.state.images.map((img,index)=> {
-       let icon = {uri: this.state.icons[img.type]};
-       return (
-         <View key={index}>
-         <VrButton onClick={()=>this.go(img)}>
+        {(this.state.start > 0)
+        ?<VrButton onClick={()=> this.back()}>
            <Image
-           source={icon}
-           style={{width: 0.5,
-                   height: 0.5,
-                   transform: [{translate: [-3.5, 0.8, -3]},
-                               {rotateY : 0},
-                               {rotateX : 0}]}} />
-          </VrButton>
-        <View><Text> {img.name}  </Text></View>
-        </View>)
-     })}
+            source={{uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6vguGtVqUlynlSkdS0xu_Nhhf8UMJd69YBILsNUnLM4TLPa1x'}}
+            style={{width: 0.3,
+                    height: 0.3,
+                    transform: [{translate: [-3.4, 2.5, -3]},
+                                {rotateY: 0}]}} />
+         </VrButton>
+        :<VrButton></VrButton>
+       }
+
+
         <Text
           style={{
             color: 'white',
@@ -94,7 +135,7 @@ export default class reactVR extends React.Component {
             paddingRight: 0.2,
             textAlign: 'center',
             textAlignVertical: 'center',
-            transform: [{translate: [0, 2, -3]}]
+            transform: [{translate: [0, 3, -3]}]
           }}>
           {this.state.roomName}
         </Text>
@@ -103,31 +144,65 @@ export default class reactVR extends React.Component {
     );
   }
 
-  tes(){
-    console.log('halo');
-  }
-
-  go(img){
+  closeDesc(){
     this.setState({
-      roomName: img.name,
-      room: img.image
+      desc:""
     })
   }
 
-  move(){
-    this.setState({
-      roomName: this.state.images[this.state.index + 1].name,
-      room: this.state.images[this.state.index + 1].image,
-      index: this.state.index + 1
-    })
-  }
   back(){
     this.setState({
-      roomName: this.state.images[this.state.index - 1].name,
-      room: this.state.images[this.state.index - 1].image,
-      index: this.state.index - 1
+      start: this.state.start - this.state.limit,
+      space: this.state.start
     })
   }
+
+  next(){
+    this.setState({
+      start: this.state.space,
+      space: this.state.space + this.state.limit
+    })
+  }
+
+  go(img,index){
+    console.log(img.name);
+    console.log(img.image);
+    console.log(index);
+    this.setState({
+      roomName: img.name,
+      room: img.image,
+      index: index,
+      desc: ""
+    })
+  }
+
+  desc(i){
+    if(this.state.desc === ""){
+      this.setState({
+        desc: this.state.images[i].description
+      })
+        console.log(i);
+    } else{
+      this.setState({
+        desc: ""
+      })
+    }
+  }
+
+  // move(){
+  //   this.setState({
+  //     roomName: this.state.images[this.state.index + 1].name,
+  //     room: this.state.images[this.state.index + 1].image,
+  //     index: this.state.index + 1
+  //   })
+  // }
+  // back(){
+  //   this.setState({
+  //     roomName: this.state.images[this.state.index - 1].name,
+  //     room: this.state.images[this.state.index - 1].image,
+  //     index: this.state.index - 1
+  //   })
+  // }
 };
 
 
