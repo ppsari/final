b5e7844e5921b5c441bce60ee4910a43656a1b12
stedyyulius <stedyyulius@gmail.com>
2017-07-14 15:55:56 +0700

    baru fungsi edit dan redux thunk di store

--- client-web/client-web/src/actions/index.js
+++ client-web/client-web/src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 export const getRentDataAction = (data) => {
   return {
     type: 'GET_DATA_RENT',
@@ -8,5 +10,20 @@ export const getSellDataAction = (data) => {
   return {
     type: 'GET_DATA_SELL',
     payload: data,
+  }
+}
+
+export const editProfile = (data) =>{
+  return (dispatch) =>{
+    axios.get('')
+    .then(response=>{
+      dispatch({
+        type: 'Edit Profile',
+        payload: response.data
+      })
+    })
+    .catch(err=>{
+      console.log(err);
+    })
   }
 }
