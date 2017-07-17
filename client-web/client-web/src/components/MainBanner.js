84f932898b8b47398c386877553cbe6ef31cc472
Priambodo-Kurniawan <iam@calibreworks.co.id>
2017-07-17 22:27:00 +0700

    home banner

--- client-web/client-web/src/components/MainBanner.js
+++ client-web/client-web/src/components/MainBanner.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Jumbotron, Button } from 'reactstrap';
 
+import './MainBanner.css'
 const MainBanner = () => {
   return (
     <div className="MainBanner">
       <Jumbotron>
         <div className="container jumbotron">
-          <div className="col-6">
-            <h1 className="display-3">Hello, world!</h1>
-            <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
-            <hr className="my-2" />
-            <p>Home. Not Protected. Anyone can see this.</p>
-            <p className="lead">
-              <Button color="primary">Learn More</Button>
-            </p>
+          <div className="row">
+            <div className="col-6 text-right">
+              <h1 className="display-3 m-t-50"><span className="extra-bold green">ROOM360</span></h1>
+              <h3 className="lead" style={{letterSpacing: 2, lineHeight: '1.7em'}}>Taking a Marketplace of property to the whole new level with <span className="extra-bold green">VIRTUAL REALITY.</span></h3>
+              <hr className="my-2" />
+              <p className="lead" >
+                <Button color="theme-btn btn-style-one btn-same m-t-30">Learn More</Button>
+              </p>
+            </div>
+            <div className="col-6">
+              <img src="/img/room.jpg" alt="" className="img-responsive"/>
+            </div>
           </div>
         </div>
       </Jumbotron>
