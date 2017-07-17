69d270de4ea1b3d7d3dd7e18a562f6a5292d14a7
ppsari <poppymighty@gmail.com>
2017-07-17 12:45:10 +0700

    jadi atau

--- server/controllers/propertyRentCtrl.js
+++ server/controllers/propertyRentCtrl.js
@@ -165,7 +165,7 @@ const searchPropsENull = (req,res) => {
       let regProp = new RegExp(req.query.prop, 'i')
       let filtered = properties.filter(property => {
 
-        if ( regCity.test(property.city) || (regProp.test(property._categoryId.name) || regProp.test(property.name)) ) {
+        if ( regCity.test(property.city)||(regProp.test(property._categoryId.name) || regProp.test(property.name)) ) {
           return property
         }
       })
