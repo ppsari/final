f0d8dc24c16623f8c5a59cddda92f47c3b39491b
Priambodo-Kurniawan <iam@calibreworks.co.id>
2017-07-18 16:33:32 +0700

    loader component

---
+++ client-web/client-web/src/components/Loader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Transaction extends React.Component {
+  render () {
+    return (
+      <div className="flex-loader">
+        <div className="loader"></div>
+      </div>
+    )
+  }
+}
