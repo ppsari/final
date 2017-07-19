0d5b730114aa73f7113f18bed3ad44ab10c9c038
Priambodo-Kurniawan <iam@calibreworks.co.id>
2017-07-19 10:03:12 +0700

    add new property and request

--- client-web/client-web/src/components/AddNewProperty.js
+++ client-web/client-web/src/components/AddNewProperty.js
@@ -208,7 +208,7 @@ class AddNewProperty extends React.Component {
                 </div>
                 <div className ="col-md-10 offset-lg-1">
                 <GoogleMapReact
-                  style={{width:50, height:250,margin:10}}
+                   style={{width:50, height:250,margin:10}}
                    center={{lat: this.state.lat, lng: this.state.lng}}
                    zoom={this.state.zoom}
                    onClick={(e)=> this.getDot(e)}
